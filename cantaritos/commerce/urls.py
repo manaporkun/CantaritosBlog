309f4dbeeffeae1d853a2525fade7bb9daf5c36e
Orkun Manap <manaporkun@gmail.com>
2021-03-18 22:51:05 +0200

    Django Update 2

--- cantaritos/commerce/urls.py
+++ cantaritos/commerce/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='cantaritos-register'),
+    path('register/', views.register, name='cantaritos-register'),
+    path('login/', views.login, name='cantaritos-login'),
 ]
--- cantaritos/commerce/views.py
+++ cantaritos/commerce/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def register(request):
-    return HttpResponse('<h1>Register</h1>')
+    return render(request, 'commerce/signup.html')
+
+
+def login(request):
+    return render(request, 'commerce/login.html')
