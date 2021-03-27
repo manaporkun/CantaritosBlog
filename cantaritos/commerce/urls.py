9917ade93ca1fad333dbf6fbe8a7531b5237afcc
Orkun Manap <manaporkun@gmail.com>
2021-03-27 22:19:08 +0200

    Profile Page Update #1

--- cantaritos/commerce/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='cantaritos-register'),
-    path('login/', views.login, name='cantaritos-login'),
-]
--- cantaritos/commerce/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def register(request):
-    return render(request, 'commerce/signup.html')
-
-
-def login(request):
-    return render(request, 'commerce/login.html')
