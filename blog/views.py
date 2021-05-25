27d465ac4b43f360c270b53f448e4484ff049c1e
Orkun Manap <manaporkun@protonmail.com>
2021-05-25 19:51:32 +0300

    Search Bar Update

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
@@ -15,6 +16,11 @@ def home(request):
     context = {
         'posts': Post.objects.all()
     }
+
+    if request.GET:
+        query = request.GET['q']
+        context['query'] = str(query)
+
     return render(request, 'blog/home.html', context)
 
 
@@ -25,6 +31,7 @@ class PostListView(ListView):
     ordering = ['-date_posted']
     paginate_by = 5
 
+
 class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
@@ -32,8 +39,9 @@ class UserPostListView(ListView):
     paginate_by = 5
 
     def get_queryset(self):
-        user = get_object_or_404(User, username = self.kwargs.get('username'))
-        return Post.objects.filter(author = user).order_by('-date_posted')
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -76,3 +84,18 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+
+def get_blog_queryset(query=None):
+    queryset = []
+    queries = query.split(" ")
+
+    for q in queries:
+        posts = Post.objects.filter(
+            Q(title__icontrains=q) |
+            Q(body__icontrains=q)).distinct()
+
+        for post in posts:
+            queryset.append(post)
+
+    return list(set(queryset))
